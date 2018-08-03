92576827eff84e1ff093fb4aab2b3d361cb47d4c
William Sterling <william.sterling@pinnacol.com>
2018-08-03 11:43:44 -0600

    ADding browser animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
+import { AlertBarComponent } from './alert-bar/alert-bar.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertBarComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MaterialModule
   ],
   providers: [],
