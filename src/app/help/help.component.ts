82a1d39a5ea54a9991999366dbfcd6a891fb0ed4
William Sterling <william.sterling@pinnacol.com>
2018-08-06 10:18:41 -0600

    Getting contacts to iterate

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactList } from './contact-list';
 
 @Component({
   selector: 'app-help',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./help.component.scss']
 })
 export class HelpComponent implements OnInit {
+  contacts = [
+    new ContactList(1, 'Dad', '/assets/images/avatar-pops.png', '303-777-6894'),
+    new ContactList(2, 'Mom', '/assets/images/avatar-mom.png', '303-912-6658'),
+    new ContactList(3, 'Poppy', '/assets/images/avatar-dad.png', '906-844-1332'),
+    new ContactList(4, '911', '/assets/images/avatar-911.png', '911')
+  ];
+  // contactList = this.contacts[0];
 
   constructor() { }
 
