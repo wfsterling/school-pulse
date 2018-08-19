2f9a5360e3d5a4faedeb2e8026f10b9b081ee731
William Sterling <william.sterling@pinnacol.com>
2018-08-19 14:24:43 -0400

    toying with svg ideas

--- src/app/checkin/checkin.module.ts
+++ src/app/checkin/checkin.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AiBubbleComponent } from './ai-bubble/ai-bubble.component';
 import { MoodSelectComponent } from './mood-select/mood-select.component';
 import {MatSliderModule} from '@angular/material/slider';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   imports: [
